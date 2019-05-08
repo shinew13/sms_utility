a9e8ccd33cbab0c4a1f0582e26d8e8958f778eb8
jingyanwang <jywang.ieee@gmail.com>
2019-05-08 16:51:11 +0400

    Update sms_utility_rest_api.py

--- sms_utility_rest_api.py
+++ sms_utility_rest_api.py
@@ -2,9 +2,6 @@
 from flask import *
 from flask_restplus import *
 
-from sms_utility_re import *
-from sms_utility_dl_model import *
-
 text_entities_struct = {\
 	'text': fields.String,\
 	'number': fields.List(fields.String),\
@@ -36,33 +33,5 @@ text_entities_struct_comb = {\
 
 text_entities_struct_full = text_entities_struct.copy()
 text_entities_struct_full.update(text_entities_struct_comb)
-
-'''
-'''
-def text_entity_list_categorization_rest_api(\
-	input,
-	indicator_func = None, 
-	indicator_list = None,
-	model = None):
-	output = []
-	for text_entity in input:
-		indicator = text_entity_categorization(text_entity,
-			indicator_func = indicator_func,\
-			indicator_list = indicator_list)
-		if model is not None:
-			prediction, score = text_entity_categorization_dl(\
-				text_entity, model)
-		if model is not None:
-			if indicator is not None or prediction > 0:
-				output.append({\
-					'entity': text_entity2entity(text_entity),\
-					'indicator': indicator,\
-					'score':score})
-		else:
-			if indicator is not None:
-				output.append({\
-					'entity': text_entity2entity(text_entity),\
-					'indicator': indicator})
-	return output
 
 ##############sms_utility_rest_api.py##############
