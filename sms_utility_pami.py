950cd55c62c84c0c8ffe2c87a3e20faa88937a29
jingyanwang <jywang.ieee@gmail.com>
2019-05-08 16:50:26 +0400

    Create sms_utility_pami.py

---
+++ sms_utility_pami.py
@@ -0,0 +1,30 @@
+from sms_utility_re import *
+from sms_utility_dl_model import *
+
+'''
+'''
+def text_entity_list_categorization_rest_api(\
+	input,
+	indicator_func = None, 
+	indicator_list = None,
+	model = None):
+	output = []
+	for text_entity in input:
+		indicator = text_entity_categorization(text_entity,
+			indicator_func = indicator_func,\
+			indicator_list = indicator_list)
+		if model is not None:
+			prediction, score = text_entity_categorization_dl(\
+				text_entity, model)
+		if model is not None:
+			if indicator is not None or prediction > 0:
+				output.append({\
+					'entity': text_entity2entity(text_entity),\
+					'indicator': indicator,\
+					'score':score})
+		else:
+			if indicator is not None:
+				output.append({\
+					'entity': text_entity2entity(text_entity),\
+					'indicator': indicator})
+	return output
