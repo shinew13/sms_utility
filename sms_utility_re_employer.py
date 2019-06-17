107fc77d44c700955b093e478e627f651758e0de
jingyanwang <jywang.ieee@gmail.com>
2019-06-17 22:58:01 +0400

    Update sms_utility_re_employer.py

--- sms_utility_re_employer.py
+++ sms_utility_re_employer.py
@@ -18,7 +18,7 @@ re_regards_employer = r'(regards|regard|regd)'
 
 re_thanks_employer = r'(thanks|thank you|thank you very much|sincerely)'
 
-re_am_employer = r'(_puntuation_ m|am)'
+re_am_employer = r'(_puntuation_ m|am|this is)'
 
 re_sender_employer_context1 = r' '\
 	+re_i_employer+r' '\
@@ -42,20 +42,21 @@ re_sender_employer_context3 = r' '\
 re_sender_employer_context4 = r' '\
 	+re_i_employer+r' '\
 	+re_be_employer+r' '\
-	+r'((\w+) ){0,2}'\
-	+'from ((\w+) ){0,2}_entity_ '
+	+r'((\w+) ){0,3}'\
+	+'from ((\w+) ){0,3}_entity_ '
 
 re_sender_employer_context5 = r' '\
 	+re_am_employer+r' '\
 	+r'((\w+) ){0,2}'\
-	+'from ((\w+) ){0,2}_entity_ '
+	+'(from ((\w+) ){0,2})*'\
+	+r'(_puntuation_ )*_entity_ '
 
 re_sender_employer_context6 = r' _name_ '\
 	+'from _entity_ here '
 
-re_sender_employer_context7 = r' (here|regards) from _entity_ '
+re_sender_employer_context7 = r' (here|regards) from ((\w+) ){0,2}_entity_ '
 
-re_sender_employer_context8 = r' (am|m|this) _name_ from _entity_'
+re_sender_employer_context8 = r' (am|m|this) (_name_|calling) ((\w+) ){0,1}from _entity_'
 
 re_sender_employer_context9 = r' this is ((\w+) ){0,2}of _entity_ '
 
@@ -77,13 +78,15 @@ re_sender_employer_context12 = r' '\
 	+r'((\w+) ){1,3}'\
 	+r'_entity_ _end_ '
 
-re_sender_employer_context13 = re_regards_employer+r' ((\w+) ){0,2}from _entity_'
+re_sender_employer_context13 = r' '\
+	+re_thanks_employer+r' '\
+	+r'((\w+) ){0,3}_entity_ ((\w+) ){0,2}_end_ '
 
-re_sender_employer_context14 = re_thanks_employer+r' _name_ _puntuation_ _entity_'
+re_sender_employer_context14 = re_regards_employer+r' ((\w+) ){0,2}from _entity_'
 
 re_sender_employer_context15 = re_regards_employer+r' _name_ _puntuation_ _entity_'
 
-max_ix = 12
+max_ix = 13
 re_sender_employer_context = []
 for ix in range(1,max_ix+1):
 	eval("re_sender_employer_context."+"append"+"(re_sender_employer_context"+str(ix)+")")
@@ -98,7 +101,7 @@ def sender_employer_context_match(input):
 			return output
 	return input
 
-input = u"  regards varadharaja _entity_ _end_  "
+input = u" this is shibili _name_ _number_ from _entity_  "
 sender_employer_context_match(input)
 
 ##################sms_utility_re_employer.py################
