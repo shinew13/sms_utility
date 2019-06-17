0f31177176c131842d3882c0b772fbb40db0b15b
jingyanwang <jywang.ieee@gmail.com>
2019-06-17 18:32:47 +0400

    Update sms_utility_re_employer.py

--- sms_utility_re_employer.py
+++ sms_utility_re_employer.py
@@ -12,9 +12,11 @@ re_in_employer = r'(at|in|with|for)'
 
 re_company_employer = r'(company|employer|institute)'
 
-re_be_employer = r'(is|was|has been|will be)'
+re_be_employer = r'(is|was|has been|will be|m)'
 
 re_regards_employer = r'(regards|regard|regd)'
+
+re_thanks_employer = r'(thanks|thank you|thank you very much|sincerely)'
 
 re_am_employer = r'(_puntuation_ m|am)'
 
@@ -48,13 +50,30 @@ re_sender_employer_context5 = r' '\
 re_sender_employer_context6 = r' _name_ '\
 	+'from _entity_ here '
 
-re_sender_employer_context = [\
-	re_sender_employer_context1,\
-	re_sender_employer_context2,\
-	re_sender_employer_context3,\
-	re_sender_employer_context4,\
-	re_sender_employer_context5,\
-	re_sender_employer_context6]
+re_sender_employer_context7 = r' here from _entity_ '
+
+# by Haoran from number 8
+re_sender_employer_context8 = r'_name_ from _entity_'
+
+re_sender_employer_context9 = r'this is ((\w+) ){0,2}of _entity_'
+
+re_sender_employer_context10 = re_regards_employer+r' _name_ _entity_'
+
+re_sender_employer_context11 = re_regards_employer+r' _puntuation_ _name_ _title_ _puntuation_ _entity_'
+
+re_sender_employer_context12 = re_regards_employer+r' _name_ _puntuation_ _entity_'
+
+re_sender_employer_context13 = re_regards_employer+r' ((\w+) ){0,2}from _entity_'
+
+re_sender_employer_context14 = re_thanks_employer+r' _name_ _puntuation_ _entity_'
+
+re_sender_employer_context15 = re_thanks_employer+r' _name_ from _entity_'
+
+max_ix = 12
+re_sender_employer_context = []
+for ix in range(1,max_ix+1):
+	eval("re_sender_employer_context."+"append"+"(re_sender_employer_context"+str(ix)+")")
+
 
 def sender_employer_context_match(input):
 	for pattern in re_sender_employer_context:
@@ -67,7 +86,7 @@ def sender_employer_context_match(input):
 	return input
 
 '''
-input = u" regards _name_ from _entity_ here "
+input = u" jim here from _entity_ "
 sender_employer_context_match(input)
 '''
 ##################sms_utility_re_employer.py################
